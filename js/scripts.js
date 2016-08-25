1a81e56c9e04258719f871ea23f7d04e18cc86fc
Garrett Price and jon toler <jon@>
2016-08-25 11:37:28 -0700

    setup our framework

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+//bussiness logic
+var player = {
+  player1: "X";
+  player2: "O";
+
+}
+
+var space = {
+  space1: false;
+  space2: false;
+  space3: false;
+  space4: false;
+  space5: false;
+  space6: false;
+  space7: false;
+  space8: false;
+  space9: false;
+}
+
+var board = {
+  space1: 1-1;
+  space2: 1-2;
+  space3: 1-3;
+  space4: 2-1;
+  space5: 2-2;
+  space6: 2-3;
+  space7: 3-1;
+  space8: 3-2;
+  space9: 3-3;
+}
+
+var game = {
+  player1:;
+  player2:;
+}
+//UI logic
